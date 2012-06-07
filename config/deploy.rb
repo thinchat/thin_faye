378b2c641a8cf35756d643a180492dbbf31cc69a
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-07 15:24:37 -0400

    Change God's start

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,6 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-# set :default_environment, {
-#   'PATH' => "/home/deployer/.rbenv/shims:/home/deployer/.rbenv/bin:$PATH"
-# }
-
 namespace :deploy do
   desc "Start faye"
   task :start, roles: :app, except: {no_release: true} do
