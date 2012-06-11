166ba24252fac0b97c6f6da15675ff72aeae54e5
Edward Weng <edweng@gmail.com>
2012-06-11 10:11:00 -0400

    Fixing redis password

--- config/initializers/pulse.rb
+++ config/initializers/pulse.rb
@@ -1,6 +1,6 @@
 if ENV['RAILS_ENV'] == 'production'
   require "#{Rails.root}/config/initializers/secret.rb"
-  PULSE = ThinHeartbeat::Pulse.new(REDIS_SECRET)
+  PULSE = ThinHeartbeat::Pulse.new(REDIS_PASSWORD)
 else
   PULSE = ThinHeartbeat::Pulse.new()
 end
