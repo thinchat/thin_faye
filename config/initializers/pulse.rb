7b0c577370fbc53c10c23d42174e939a49e44c96
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-11 23:46:41 -0400

    Set password in all environments

--- config/initializers/pulse.rb
+++ config/initializers/pulse.rb
@@ -1,6 +1,2 @@
-if ENV['RAILS_ENV'] == 'production'
-  require "./config/secret/redis_password.rb"
-  PULSE = ThinHeartbeat::Pulse.new(REDIS_PASSWORD)
-else
-  PULSE = ThinHeartbeat::Pulse.new()
-end
+require "./config/secret/redis_password.rb"
+PULSE = ThinHeartbeat::Pulse.new(REDIS_PASSWORD)
