4ff5b8d038191bb42dbf11d83322a540681c486b
Edward Weng <edweng@gmail.com>
2012-06-12 16:34:10 -0400

    Adding faye password requirements

--- config/initializers/pulse.rb
+++ config/initializers/pulse.rb
@@ -1,6 +1,15 @@
 if ENV['RAILS_ENV'] == 'production'
+  REDIS_URL = 'http://thinchat.com'
+elsif ENV['RAILS_ENV'] == 'staging'
+  REDIS_URL = 'http://50.116.40.131'
+else
+  REDIS_URL = 'localhost'
+end
+
+PASSWORD_REQUIRED = ['production', 'staging']
+
+if PASSWORD_REQUIRED.include? ENV['RAILS_ENV']
   require "./config/secret/redis_password.rb"
-  PULSE = ThinHeartbeat::Pulse.new(REDIS_PASSWORD)
+  PULSE = ThinHeartbeat::Pulse.new(host, REDIS_PASSWORD)
 else
   PULSE = ThinHeartbeat::Pulse.new()
-end
--- lib/client.rb
+++ lib/client.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'active_support/core_ext'
 
 class Client
-  CLIENT_ATTRIBUTES = [:user_name, :user_id, :user_type, :room_id, :client_id, :room]
+  CLIENT_ATTRIBUTES = [:user_name, :user_id, :user_type, :location, :client_id, :channel]
   attr_reader *CLIENT_ATTRIBUTES
 
   def initialize(hash)
@@ -12,11 +12,11 @@ class Client
   end
 
   def self.new_from_message(faye_message)
-    Client.new({ "user_id" => faye_message.user_id,
+    Client.new({ "user_id"   => faye_message.user_id,
                  "user_name" => faye_message.user_name,
                  "user_type" => faye_message.user_type,
-                 "room" => faye_message.room,
-                 "room_id" => faye_message.room_id,
+                 "channel"   => faye_message.channel,
+                 "location"  => faye_message.location,
                  "client_id" => faye_message.client_id })
   end
 end
